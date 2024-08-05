1c34ddff35ce7a8d9c90d23dee3186e7edebbcff
Xueyuan Jiang <alexxyjiang@gmail.com>
2024-08-05 15:58:43 +0800

    paspc: update

--- src/paspc/rest/client.py
+++ src/paspc/rest/client.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # generic rest http asyncio client based on aiohttp
-import aiohttp
+from aiohttp import ClientSession
 from .parser import RestfulParser
 
 
 class RestfulClient(object):
 
-    def __init__(self, client_session: aiohttp.ClientSession, client_parser: RestfulParser):
+    def __init__(self, client_session: ClientSession, client_parser: RestfulParser):
         self.__client_session__ = client_session
         self.__client_parser__ = client_parser
 
-    async def request_parse(self, method: str, url: str) -> dict:
-        async with self.__client_session__.request(method, url) as response:
+    async def request_parse(self, method: str, url: str, **kwargs) -> dict:
+        async with self.__client_session__.request(method, url, **kwargs) as response:
             status = response.status
             headers = response.headers
             body = await response.text()
